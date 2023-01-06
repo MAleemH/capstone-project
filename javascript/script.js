f22b93311bf7774274a618bfa69c9866909450ef
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2023-01-06 11:30:48 +0500

    added home link into the navbar

--- javascript/script.js
+++ javascript/script.js
@@ -20,37 +20,37 @@ document.querySelectorAll('.nav-options').forEach((n) => n.addEventListener('cli
 const SpeakersList = [
   {
     speakername: 'Satya Nadella',
-    imagelink: '../images/yochai.jpg',
+    imagelink: './images/yochai.jpg',
     desc1: 'Microsoft CEO ',
     desc2: 'Satya Nadella knows a thing or two about success in business. The technology executive reportedly earned over $16 million in pay last year',
   },
   {
     speakername: 'Bill Gates',
-    imagelink: '../images/sohyoeng.jpg',
+    imagelink: './images/sohyoeng.jpg',
     desc1: 'Co-founder of Microsoft',
     desc2: 'William Henry Gates III is an American business magnate, software developer, investor, author, and philanthropist. He is a co-founder of Microsoft',
   },
   {
     speakername: 'Linus Torvalds',
-    imagelink: '../images/sohyoeng.jpg',
+    imagelink: './images/sohyoeng.jpg',
     desc1: 'Founder of Linux',
     desc2: 'Linus Benedict Torvalds is a Finnish-American software engineer who is the creator and the lead developer of the Linux kernel. He also created the distributed version control system Git.',
   },
   {
     speakername: 'Mark Zuckerberg',
-    imagelink: '../images/yochai.jpg',
+    imagelink: './images/yochai.jpg',
     desc1: 'CEO of FaceBook',
     desc2: 'Mark Elliot Zuckerberg is an American business magnate, internet entrepreneur, and philanthropist. He is known for co-founding the social media website Facebook',
   },
   {
     speakername: 'Brian Chesky',
-    imagelink: '../images/yochai.jpg',
+    imagelink: './images/yochai.jpg',
     desc1: 'CEO of airbnb',
     desc2: 'Brian Joseph Chesky is an American businessman and industrial designer. He is the co-founder and CEO of the peer-to-peer lodging service Airbnb.',
   },
   {
     speakername: 'Sundar Pichai',
-    imagelink: '../images/sohyoeng.jpg',
+    imagelink: './images/sohyoeng.jpg',
     desc1: 'CEO of Google',
     desc2: 'Pichai Sundararajan, better known as Sundar Pichai, is an Indian-American business executive..',
   },
